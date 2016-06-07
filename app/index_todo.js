3bf0a1e1f6ed3d71795265d57d921e9414aaabdc
prabhnith <prabhjot.nith@gmail.com>
2016-06-07 15:13:25 +0530

    Intial working commit

---
+++ app/index_todo.js
@@ -0,0 +1,65 @@
+var React =  require('react');
+var ReactDOM=require('react-dom');
+
+var TodoContainer = React.createClass({
+  getInitialState : function(){
+    return{
+      list : ['TODO #1','TODO #2','TODO #3'],
+    }
+  },
+  addItem: function(item){
+    this.setState({
+      list : this.state.list.concat([item]);
+    });
+  },
+  render : function(){
+    return(
+      <div>
+        <h3> TODO LIST :</h3>
+        <AddItem addItem={this.addItem} />
+        <ShowList items={this.state.list} />
+      </div>
+    )
+  }
+});
+
+var AddItem = React.createClass({
+  getInitialState: function(){
+    return{
+      newItem:''
+    }
+  },
+  handleChange : function(e){
+    this.setState({
+      addItem: e.target.value
+    });
+  },
+  handleAddItem: function(){
+    e.preventDefault();
+    this.props.addItem(this.state.newItem);
+    this.setState({
+      newItem:''
+    });
+  },
+  render: function(){
+    return (
+      <div>
+        <input type="text" value={this.state.addItem} onChange={this.handleChange} />
+        <button onClick={this.handleAddItem}> Add Item</button>
+      </div>
+    );
+  }
+});
+var ShowList = React.createClass({
+  render : function(){
+    var listItems = this.props.items.map(function(item){
+      return <li> {item} </li>;
+    });
+    return(
+      <dic>
+        <ul> {listItems} </ul>
+      </dic>
+    )
+  }
+});
+ReactDOM.render(<TodoContainer />, document.getElementById('app'));
