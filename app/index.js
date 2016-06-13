72e8601536ba97d066db77d8e2a186debe76bb5d
prabhnith <prabhjot.nith@gmail.com>
2016-06-13 14:29:40 +0530

    update in web-socket connection re-establishment

--- app/index.js
+++ app/index.js
@@ -1,65 +1,75 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 var uuid = require('uuid');
-ws = new WebSocket('ws://192.168.1.117:3000/v5/ws');
-// var ws = new WebSocket('ws://localhost:8008/ws');
-
-setupWsHandle(ws);
-function setupWsHandle(ws){
+pendingData = [];
+// ws = new WebSocket('ws://192.168.1.117:3000/v5/ws');
+// ws = new WebSocket('ws://127.0.0.1:8008/ws');
+wsocks = {connection: new WebSocket('ws://127.0.0.1:8008/ws') }
+setupWsHandle();
+function setupWsHandle(){
 	console.log("Trying..");
-	ws.onopen = () => {
+	// ws = new WebSocket('ws://127.0.0.1:8008/ws');
+	wsocks.connection.onopen = () => {
 			console.log("connection established");
-			var sendToWs=JSON.stringify(todos);
-			ws.send(sendToWs);
-			console.log("data send intially");
+			console.log(wsocks.connection.readyState);
+			var sendToWs=JSON.stringify(pendingData);
+			wsocks.connection.send(sendToWs);
+			pendingData=[];
 	};
-	ws.onmessage = (e) => {
+	wsocks.connection.onmessage = (e) => {
 	console.log("This is the received data " );
 	console.log( e.data);
  };
-	ws.onmessage = (e) => {
+	wsocks.connection.onmessage = (e) => {
 			console.log(e.data);
 	};
-	ws.onclose = (e) => {
+	wsocks.connection.onclose = (e) => {
 			console.log(e.code, e.reason);
 			console.log("connection dropped but changes will be saved to local storage");
 			console.log("Retrying connection...");
-			var wss = new WebSocket('ws://192.168.1.117:3000/v5/ws');
-			// var wss = new WebSocket('ws://localhost:8008/ws');
-			setupWsHandle(wss);
+			console.log(wsocks.connection.readyState);
+			try {
+			    wsocks.connection = new WebSocket('ws://127.0.0.1:8008/ws');
+			}
+			catch(err) {
+			    console.log("connection can't be established");
+			}
+			setupWsHandle();
 		}
 };
-
 var TodoBox = React.createClass({
 	getInitialState: function () {
 		return {
 			data: this.props.todos
 		};
 	},
-	//receive data from the url
-	// loadTodosFromServer: function() {}
-	// componentDidMount: function(){
-	// 	this.loadTodosFromServer();
-	// 	setInterval(this.loadTodosFromServer, this.props.refreshInterval)
-	// },
 
 	generateId: function () {
 		return uuid.v4();
 	},
+
 
 	handleNodeRemoval: function (nodeId) {
 		var data = this.state.data;
 		var elementToSend = data.filter(function (element) {
 			return element.id === nodeId;
 		});
-
 		elementToSend[0]["status"]="delete";
-		var sendToWs=JSON.stringify(elementToSend);
-		ws.send(sendToWs);
+
+		try {
+			var sendToWs=JSON.stringify(elementToSend);
+			wsocks.connection.send(sendToWs);
+		}
+		catch(err) {
+		    pendingData = pendingData.concat(elementToSend);
+				console.log(wsocks.connection.readyState);
+				console.log(err);
+		}
 
 		data = data.filter(function (element) {
 			return element.id !== nodeId;
-		});
+		}
+	);
 
 		this.setState({data});
 		localStorage.setItem('todos', JSON.stringify(data));
@@ -72,8 +82,18 @@ var TodoBox = React.createClass({
 		var status = "insert"
 
 		elementToSend = [{id, task, complete,status}];
-		var sendToWs=JSON.stringify(elementToSend);
-		ws.send(sendToWs);
+		
+		try {
+			var sendToWs=JSON.stringify(elementToSend);
+			wsocks.connection.send(sendToWs);
+		}
+		catch(err) {
+		    pendingData = pendingData.concat(elementToSend);
+				console.log(wsocks.connection.readyState);
+				console.log(err);
+		}
+				// var sendToWs=JSON.stringify(elementToSend);
+		// wsocks.connection.send(sendToWs);
 
 		data = data.concat([{id, task, complete,status}]);
 		this.setState({data});
@@ -93,8 +113,19 @@ var TodoBox = React.createClass({
 		this.setState({data});
 		localStorage.setItem('todos', JSON.stringify(data));
 		var elementToSend = data[position];
-		var sendToWs=JSON.stringify(elementToSend);
-		ws.send(sendToWs);
+		try {
+			var sendToWs=JSON.stringify(elementToSend);
+			wsocks.connection.send(sendToWs);
+		}
+		catch(err) {
+				pendingData = pendingData.concat(elementToSend);
+				console.log(wsocks.connection.readyState);
+				console.log(err);
+		}
+
+
+		// var sendToWs=JSON.stringify(elementToSend);
+		// wsocks.connection.send(sendToWs);
 		return;
 	},
 	render: function() {
