4f97b84204c8934fa746db440af7475f6e7d2b0d
ashu01 <as01hu@gmail.com>
2016-06-15 09:27:21 +0530

    client updated

--- todoclient.go
+++ todoclient.go
@@ -1,36 +1,35 @@
 package main
 
 import (
-        "flag"
-    	"log"
-	    "net/url"
-	    "os"
-	    "os/signal"
-	    // "time"
+	"flag"
+	"log"
+	"net/url"
+	"os"
+	"os/signal"
+	// "time"
 
-	    "github.com/gorilla/websocket"
-        // "golang.org/x/net/websocket"
-        "fmt"
-        "encoding/json"
-        // "time"
-    )
+	"github.com/gorilla/websocket"
+	// "golang.org/x/net/websocket"
+	"encoding/json"
+	"fmt"
+	// "time"
+)
 
 type Task struct {
 	ID       string `json:"id,omitempty"`
-	Content     string `json:"content,omitempty"`
+	Content  string `json:"content,omitempty"`
 	Complete bool   `json:"complete"`
 }
 
-type WSMessage struct{
- MessageData []Task `json:"messageData,omitempty"`
- MessageLabel string `json:"messageLabel"`
+type WSMessage struct {
+	MessageData  []Task `json:"messageData,omitempty"`
+	MessageLabel string `json:"messageLabel"`
 }
-
 
 var addr = flag.String("addr", "localhost:3000", "http service address")
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 	log.SetFlags(0)
 
 	interrupt := make(chan os.Signal, 1)
@@ -44,37 +43,41 @@ func main() {
 		log.Fatal("dial:", err)
 	}
 	defer c.Close()
-    
-    var wsmessages []WSMessage
-    var message_data []Task
 
+	// var wsmessages WSMessage
+	var message_data []Task
+	task := Task{
+		ID:       "eswfwrfwrf",
+		Content:  "repeat",
+		Complete: false,
+	}
+	message_data = append(message_data, task)
+	task = Task{
+		ID:       "fwfwrfwrfd",
+		Content:  "sleep",
+		Complete: false,
+	}
+	message_data = append(message_data, task)
+	task = Task{
+		ID:       "aswfwrfwrfd",
+		Content:  "eat",
+		Complete: false,
+	}
+	message_data = append(message_data, task)
 
+	ws1 := WSMessage{
+		MessageData:  message_data,
+		MessageLabel: "insert",
+	}
 
-    task := Task{
-            ID : "wfwfwrfwrf",
-            Content : "ewfwrfrgrgerg",
-            Complete : true,
-    }
-    message_data = append(message_data, task) 
-    task = Task{
-            ID : "wfwfwrfwrfd",
-            Content : "ewfwrfdrgrgerg",
-            Complete : true,
-    }
-    message_data = append(message_data, task) 
-    ws1 := WSMessage{
-        MessageData : message_data,
-        MessageLabel: "update",
-    }
+	// wsmessages = append(wsmessages,ws1)
 
-    wsmessages = append(wsmessages,ws1)
+	// fmt.Println(wsmessages)
 
-    // fmt.Println(wsmessages)
-
-    b,err1 := json.Marshal(wsmessages[0])
-    if err1 != nil {
+	b, err1 := json.Marshal(ws1)
+	if err1 != nil {
 		fmt.Println(" error : ", err1)
 	}
-    fmt.Println(string(b))
-    c.WriteMessage(1,b)    
+	fmt.Println(string(b))
+	c.WriteMessage(1, b)
 }
